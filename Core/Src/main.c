6b9cd2da3a2e5c150f573826bc9e23bbb8de25dc
saloutos <saloutos96@gmail.com>
2023-05-16 15:04:51 -0400

    upping SPI baud to 11Mbps

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,7 +63,7 @@
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
-#define VERSION_NUM 3.10f // incremented to 3.01 by Elijah for CAN retransmission 5/6/23
+#define VERSION_NUM 3.11f // incremented to 3.01 by Elijah for CAN retransmission 5/6/23
 
 
 /* USER CODE END PM */
